47bc5c1ed33b96f04e6e3b3d10c1ede3d4d37e70
maximlopin <wzzhz13@gmail.com>
2019-12-04 16:01:28 +0300

    get AffineTransform working

--- transforms.py
+++ transforms.py
@@ -4,86 +4,15 @@ from PIL import Image, ImageDraw
 
 
 class TransformBase():
+    """Transform Interface"""
+
     def __call__(self, X):
+        """
+        Parameters:
+            image: np.ndarray of shape (H, W, 3)
+            boxes: np.ndarray of shape (N, 2)
+        """
         raise NotImplementedError
-
-
-class Rotate(TransformBase):
-    def __init__(self, deg):
-        """
-        Parameters:
-            deg: tuple(scalar, scalar) or scalar
-        """
-        if isinstance(deg, tuple):
-            self.min, self.max = deg
-        elif isinstance(deg, (int, float)):
-            self.min, self.max = -deg / 2, deg / 2
-        else:
-            raise TypeError('deg must be either tuple or scalar')
-
-
-    def __call__(self, image, boxes):
-        """
-        Parameters:
-            image: np.ndarray of shape (H, W, 3)
-            boxes: np.ndarray of shape (N, 2)
-        """
-        deg = np.random.uniform(self.min, self.max)
-
-        rad = np.deg2rad(deg)
-
-        c, s = np.cos(rad), np.sin(rad)
-        w, h = image.shape[1] / 2, image.shape[0] / 2
-
-        image_out = ndimage.rotate(image, -deg, reshape=False)
-        boxes_out = np.dot([[c, -s], [s, c]], (boxes - [w, h]).T)
-
-        return image_out, boxes_out.T + [w, h]
-
-
-class Shear(TransformBase):
-    def __init__(self, x_shear, y_shear):
-        """
-        Parameters:
-            x_shear: tuple(scalar, scalar) or scalar
-            y_shear: tuple(scalar, scalar) or scalar
-        """
-        if isinstance(x_shear, tuple):
-            self.xmin, self.xmax = x_shear
-        elif isinstance(x_shear, (int, float)):
-            self.xmin, self.xmax = -x_shear / 2, x_shear / 2
-        else:
-            raise TypeError('x_shear must be either tuple or float')
-
-        if isinstance(y_shear, tuple):
-            self.ymin, self.ymax = y_shear
-        elif isinstance(y_shear, (int, float)):
-            self.ymin, self.ymax = -y_shear / 2, y_shear / 2
-        else:
-            raise TypeError('y_shear must be either tuple or float')
-
-
-    def __call__(self, image, boxes):
-        """
-        Parameters:
-            image: np.ndarray of shape (H, W, 3)
-            boxes: np.ndarray of shape (N, 2)
-        """
-        a = np.random.uniform(self.xmin, self.xmax)
-        b = np.random.uniform(self.ymin, self.ymax)
-
-        m = np.array([[1, b, 0],
-                      [a, 1, 0],
-                      [0, 0, 1]])
-
-        w, h = image.shape[1], image.shape[0]
-
-        o = np.array([w, h]) * [-b / 2, -a / 2]
-
-        image_out = ndimage.affine_transform(image, m, [*o, 0])
-        boxes_out = np.dot([[1, -a], [-b, 1]], boxes.T).T + [-o[1], -o[0]]
-
-        return image_out, boxes_out
 
 
 class AdaptiveCrop(TransformBase):
@@ -101,8 +30,12 @@ class Compose(TransformBase):
         self.tfms = tfms
 
     def __call__(self, image, boxes):
+        if isinstance(image, Image.Image):
+            image = np.asarray(image)
+
         for t in self.tfms:
             image, boxes = t(image, boxes)
+
         return image, boxes
 
 
@@ -120,15 +53,65 @@ class Flip(TransformBase):
 
 
 class ToPIL(TransformBase):
-    def __init__(self):
-        pass
-
     def __call__(self, image, boxes):
         image_out = Image.fromarray(image)
 
         draw = ImageDraw.Draw(image_out)
         for b in boxes:
-            print([*b])
-            draw.ellipse([*(b - 20), *(b + 20)], fill=(255, 0, 255), width=5)
+            x, y = b
+            draw.ellipse([x-20, y-20, x+20, y+20], fill=(255, 0, 255), width=5)
 
         return image_out, boxes
+
+
+class RandomAffine(TransformBase):
+    def __init__(self, deg, shx, shy):
+        self.deg = deg
+        self.shx = shx
+        self.shy = shy
+        self.mode = 'constant'
+    
+    def __call__(self, image, boxes):
+
+        rad = np.deg2rad(self.deg)
+
+        h, w, _ = image.shape
+
+        # Translate origin to center
+        translate_1 = np.identity(4)
+        translate_1[0, -1] = h / 2
+        translate_1[1, -1] = w / 2
+
+        # Rotate
+        rotate = np.identity(4)
+        rotate[0, 0] = np.cos(rad)
+        rotate[0, 1] = np.sin(rad)
+        rotate[1, 0] = -np.sin(rad)
+        rotate[1, 1] = np.cos(rad)
+
+        # Translate center to origin
+        translate_2 = np.identity(4)
+        translate_2[0, -1] = -h / 2 - (self.shy * h) / 2
+        translate_2[1, -1] = -w / 2 - (self.shx * w) / 2
+
+        # Shear
+        shear = np.identity(4)
+        shear[0, 1] = self.shy
+        shear[1, 0] = self.shx
+
+        # Final image transform matrix
+        img_tfm = translate_1 @ rotate @ translate_2 @ shear
+
+        # Final boxes transform matrix
+        idx = [0, 1, 3]
+        box_tfm = np.linalg.inv(img_tfm[idx][:, idx])
+
+        # [[x, y], ...] -> [[y, ...], [x, ...], [1.0, ...]]
+        n = boxes.shape[0]
+        boxes = np.concatenate([boxes.T[::-1], np.ones(n).reshape(1, -1)], 0)
+
+        # Apply transforms
+        image_out = ndimage.affine_transform(image, img_tfm)
+        boxes_out = (box_tfm @ boxes)[:-1][::-1].T
+        
+        return image_out, boxes_out
