f0fb01bf2e31ad4e3db103aa99ff387aeca5157b
maximlopin <wzzhz13@gmail.com>
2019-12-05 21:48:49 +0300

    clean up, add requirements, licence, make datasets.LabelImgDataset more generic

--- boxaug/datasets.py
+++ boxaug/datasets.py
@@ -2,37 +2,33 @@ import os
 import numpy as np
 from PIL import Image
 from lxml import etree
-from boxaug.utils import IOU
-import boxaug.transforms as transforms
+from boxaug import transforms
 
 
 class LabelImgDataset():
-    def __init__(self, path, use_labels, anchors, transform=None):
+    def __init__(self, path, transform=None, use_labels=None):
         """
         Args:
-            path: path to folder with labelimg .xml files
-            use_labels: labels that are not in this list will be ignored
-            anchors: list of normalized (width, height) pairs
+            path: path to folder with LabelImg .xml files
+
+            use_labels: bounding boxes whose labels are not in this list
+            will be ignored.
+
             transform: see boxaug.transforms
         """
-        samples, itos, stoi = self._load_samples(path, use_labels, anchors)
+        samples, labels_set = self._load_samples(path, use_labels)
 
-        if transform is None:
-            tfm = transforms.Identity()
-        else:
-            tfm = transform
+        itos = list(labels_set)
+        stoi = dict((x, i) for i, x in enumerate(itos))
 
         self.samples = samples
         self.itos = itos
         self.stoi = stoi
-        self.tfm = tfm
-        self.anchors = np.array(anchors)
+        self.tfm = transform or transforms.Identity()
 
-    def _load_samples(self, path, use_labels, anchors):
-        skipped_counter = 0
-
+    def _load_samples(self, path, use_labels=None):
         samples = []
-        all_labels = set()
+        labels_set = set()
 
         for filename in os.listdir(path):
             if filename.endswith('xml'):
@@ -41,8 +37,6 @@ class LabelImgDataset():
                 tree = etree.parse(xml_path)
 
                 im_path = tree.find('path').text
-                w = int(tree.find('size/width').text)
-                h = int(tree.find('size/height').text)
 
                 boxes = []
                 labels = []
@@ -54,58 +48,34 @@ class LabelImgDataset():
                     y1 = int(obj.find('bndbox/ymax').text)
                     label = obj.find('name').text
 
-                    if label in use_labels:
+                    if use_labels is None or label in use_labels:
                         boxes.append([x0, y0, x1, y1])
                         labels.append(label)
 
-                if not boxes:
-                    skipped_counter += 1
-                    continue
+                if boxes:
+                    samples.append((im_path, np.array(boxes), labels))
+                    labels_set.update(labels)
 
-                boxes = np.array(boxes)
-
-                boxes_wh = [-1.0, 1.0] @ boxes.reshape(-1, 2, 2)
-                boxes_xy = [0.5, 0.5] @ boxes.reshape(-1, 2, 2)
-
-                # Assign best (highest IOU) anchor box to each box
-                anchor_indices = []
-                for b_wh in boxes_wh / [w, h]:
-                    i = np.argmax([IOU(b_wh, a_wh) for a_wh in anchors])
-                    anchor_indices.append(i)
-
-                image = (im_path, anchor_indices, boxes_xy, labels)
-
-                samples.append(image)
-                all_labels.update(labels)
-
-        itos = list(all_labels)
-        stoi = dict((label, i) for i, label in enumerate(itos))
-
-        print('Images loaded:', len(samples))
-        print('Images skipped:', skipped_counter)
-
-        return samples, itos, stoi
+        return samples, labels_set
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, index):
-        """
-        Returns:
-            tuple(
-                (np.ndarray) image
-                (np.ndarray) center coordinates of boxes
-                (np.ndarray) widths and heights of boxes
-                (list) labels of boxes
-            )
-        """
-        im_path, anchor_indices, boxes_xy, labels = self.samples[index]
+        path, bboxes, labels = self.samples[index]
 
-        image = Image.open(im_path).convert('RGB')
+        image = Image.open(path).convert('RGB')
         image_arr = np.asarray(image)
 
-        tfm_image, tfm_boxes = self.tfm(image_arr, boxes_xy)
+        xy_array, wh_array = transforms.split_bboxes(bboxes)
 
-        anchor_boxes = np.array([self.anchors[i] for i in anchor_indices])
+        image_out, xy_array_out = self.tfm(image_arr, xy_array)
 
-        return tfm_image, tfm_boxes, anchor_boxes, labels
+        w_ratio = image_out.shape[1]/image_arr.shape[1]
+        h_ratio = image_out.shape[0]/image_arr.shape[0]
+
+        wh_array_out = wh_array * [w_ratio, h_ratio]
+
+        bboxes_out = transforms.merge_bboxes(xy_array_out, wh_array_out)
+
+        return image_out, bboxes_out, labels
--- boxaug/transforms.py
+++ boxaug/transforms.py
@@ -2,21 +2,20 @@ import numpy as np
 from scipy import ndimage
 from PIL import Image, ImageDraw
 
-__all__ = ['Compose', 'Affine', 'Identity', 'Flip', 'ToPIL']
+__all__ = ['Compose', 'Affine', 'Identity', 'Flip', 'Resize']
 
 
 class TransformBase():
     """Transform Interface"""
 
-    def __call__(self, image, boxes):
+    def __call__(self, image, points):
         """
         Args:
             image: np.ndarray of shape (H, W, 3)
-            boxes: np.ndarray of shape (N, 2)
+            points: np.ndarray of shape (N, 2)
 
-        Returns (same shapes):
-            image: np.ndarray of shape (H, W, 3)
-            boxes: np.ndarray of shape (N, 2)
+        Returns:
+            tuple(transformed image, transformed points)
         """
         raise NotImplementedError
 
@@ -68,10 +67,10 @@ class Compose(TransformBase):
     def __init__(self, *tfms):
         self.tfms = tfms
 
-    def __call__(self, image, boxes):
+    def __call__(self, image, points):
         for t in self.tfms:
-            image, boxes = t(image, boxes)
-        return image, boxes
+            image, points = t(image, points)
+        return image, points
 
 
 class Flip(TransformBase):
@@ -79,36 +78,18 @@ class Flip(TransformBase):
         assert axis in [0, 1], 'flip axis must be 0 or 1'
         self.axis = axis
 
-    def __call__(self, image, boxes):
+    def __call__(self, image, points):
         w, h = image.shape[1], image.shape[0]
 
         if np.random.random() > 0.5:
             image = np.flip(image, axis=self.axis)
 
             if self.axis == 0:
-                boxes = [1, -1] * boxes + [0, h]
+                points = [1, -1] * points + [0, h]
             else:
-                boxes = [-1, 1] * boxes + [w, 0]
+                points = [-1, 1] * points + [w, 0]
 
-        return image, boxes
-
-
-class ToPIL(TransformBase):
-    """Converts image and boxes to a PIL image"""
-
-    def __call__(self, image, boxes):
-
-        d = 0.05 * min(image.shape[0], image.shape[1])
-        fill = (255, 255, 0)
-
-        image_out = Image.fromarray(image)
-
-        draw = ImageDraw.Draw(image_out)
-        for b in boxes:
-            x, y = b
-            draw.ellipse([x-d, y-d, x+d, y+d], fill=fill, width=3)
-
-        return image_out, boxes
+        return image, points
 
 
 class Affine(TransformBase):
@@ -148,7 +129,7 @@ class Affine(TransformBase):
 
         self.mode = resampling
 
-    def __call__(self, image, boxes):
+    def __call__(self, image, points):
         shx = np.random.uniform(*self.shx)
         shy = np.random.uniform(*self.shy)
         deg = np.random.uniform(*self.deg)
@@ -182,24 +163,24 @@ class Affine(TransformBase):
         # Final image transform matrix
         img_tfm = translate_1 @ rotate @ translate_2 @ shear
 
-        # Final boxes transform matrix
+        # Final points transform matrix
         idx = [0, 1, 3]
-        box_tfm = np.linalg.inv(img_tfm[idx][:, idx])
+        point_tfm = np.linalg.inv(img_tfm[idx][:, idx])
 
         # [[x, y], ...] -> [[y, ...], [x, ...], [1.0, ...]]
-        n = boxes.shape[0]
-        boxes = np.concatenate([boxes.T[::-1], np.ones(n).reshape(1, -1)], 0)
+        n = points.shape[0]
+        points = np.concatenate([points.T[::-1], np.ones(n).reshape(1, -1)], 0)
 
         # Apply transforms
         image_out = ndimage.affine_transform(image, img_tfm, mode=self.mode)
-        boxes_out = (box_tfm @ boxes)[:-1][::-1].T
+        points_out = (point_tfm @ points)[:-1][::-1].T
 
-        return image_out, boxes_out
+        return image_out, points_out
 
 
 class Identity(TransformBase):
-    def __call__(self, image, boxes):
-        return image, boxes
+    def __call__(self, image, points):
+        return image, points
 
 
 class Resize(TransformBase):
@@ -207,13 +188,92 @@ class Resize(TransformBase):
         self.w = width
         self.h = height
 
-    def __call__(self, image, boxes):
+    def __call__(self, image, points):
         w, h = image.shape[1], image.shape[0]
 
         pil_img = Image.fromarray(image)
         pil_img = pil_img.resize((self.w, self.h), Image.BILINEAR)
         image_out = np.asarray(pil_img)
 
-        boxes_out = boxes * [self.w / w, self.h / h]
+        points_out = points * [self.w / w, self.h / h]
 
-        return image_out, boxes_out
+        return image_out, points_out
+
+
+def split_bboxes(bboxes):
+    """
+    Splits array of bounding boxes of format [[x0, y0, x1, y1], ...]
+    into 2 arrays: center coordinates of bboxes, widths and heights of bboxes
+
+    Args:
+        np.ndarray of shape (N, 4)
+
+    Returns:
+        tuple(
+            np.ndarray of shape(N, 2): [[x, y], ...]
+            np.ndarray of shape(N, 2): [[w, h], ...]
+        )
+    """
+    assert len(bboxes.shape) == 2
+    assert bboxes.shape[1] == 4
+
+    xy_tfm = np.array([[0.5, 0.0],
+                       [0.0, 0.5],
+                       [0.5, 0.0],
+                       [0.0, 0.5]])
+
+    wh_tfm = np.array([[-1.0, 0.0],
+                       [0.0, -1.0],
+                       [1.0, 0.0],
+                       [0.0, 1.0]])
+
+    xy = bboxes @ xy_tfm
+
+    wh = bboxes @ wh_tfm
+
+    return xy, wh
+
+
+def merge_bboxes(xy_array, wh_array):
+    """
+    Args:
+        xy_array: np.ndarray of shape (N, 2), center coordinates of bboxes
+        wh_array: np.ndarray of shape (N, 2), widths and heights of bboxes
+
+    Returns:
+        np.ndarray of shape (N, 4) like this: [[x0, y0, x1, y1], ...]
+    """
+    assert xy_array.shape[0] == xy_array.shape[0]
+    assert len(xy_array.shape) == len(wh_array.shape) == 2
+    assert xy_array.shape[1] == wh_array.shape[1] == 2
+
+    tfm = np.array([[1, 0, -0.5, 0],
+                    [0, 1, 0, -0.5],
+                    [1, 0, 0.5, 0],
+                    [0, 1, 0, 0.5]])
+
+    bboxes = tfm @ np.concatenate((xy_array, wh_array), axis=1).T
+
+    return bboxes.T
+
+
+def to_pil(image, bboxes):
+    """
+    Args:
+        image: np.ndarray of shape (H, W, 3)
+        bboxes: np.ndarray of shape (N, 4)
+
+    Returns:
+        PIL Image
+    """
+    assert len(bboxes.shape) == 2
+    assert bboxes.shape[1] == 4
+
+    pil_image = Image.fromarray(image)
+
+    draw = ImageDraw.Draw(pil_image)
+
+    for x0, y0, x1, y1 in bboxes:
+        draw.rectangle([x0, y0, x1, y1], outline=(255, 255, 0), width=3)
+
+    return pil_image
